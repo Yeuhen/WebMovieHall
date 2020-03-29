0ce6851d341c77c8958d1207a389c6c72d63f041
Yevhen <lyunkor@gmail.com>
2020-03-30 01:51:42 +0300

    build added

---
+++ build/precache-manifest.1593c25bbdd361d12164b8d9a24ee1bd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b78312a8245e97abe2438a7440444d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a10cf9c7d6431d51e94",
+    "url": "/static/css/main.2399070c.chunk.css"
+  },
+  {
+    "revision": "06946ee0d52cd32f6f48",
+    "url": "/static/js/2.998d9357.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.998d9357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a10cf9c7d6431d51e94",
+    "url": "/static/js/main.6927d78f.chunk.js"
+  },
+  {
+    "revision": "77dfa06e5a427415e424",
+    "url": "/static/js/runtime-main.489ead42.js"
+  },
+  {
+    "revision": "bbb6049aecc87747f60941d2d0dceee0",
+    "url": "/static/media/bg-texture.bbb6049a.jpg"
+  },
+  {
+    "revision": "78947c6e2464a3f909acb607bba3c59f",
+    "url": "/static/media/heart.78947c6e.png"
+  },
+  {
+    "revision": "a8f38891b03380ad492990646d8e9559",
+    "url": "/static/media/main-background-image.a8f38891.jpg"
+  },
+  {
+    "revision": "882ca95665a8e79eb65d87bf0f6c3232",
+    "url": "/static/media/myPhoto_1.882ca956.jpg"
+  },
+  {
+    "revision": "e1105a565c98e9f051d6719d7bcc1387",
+    "url": "/static/media/no-image-available.e1105a56.jpg"
+  },
+  {
+    "revision": "9585d6dac72e370d9624ccdc32d3d799",
+    "url": "/static/media/web-movie-hall-logo.9585d6da.png"
+  }
+]);
